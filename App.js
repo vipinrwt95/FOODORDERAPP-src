1ee7840510d640e4e424cb62435d0a13bfab0dcb
vipinrwt95 <vipinrwt9@gmail.com>
2022-11-20 12:54:03 +0530

    adding summary

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import MealsSummary from "./components/Meals/MealsSummary";
 function App() {
   return (
    <React.Fragment>
      <Header></Header>
+     <MealsSummary />
    </React.Fragment>  
 
   );
